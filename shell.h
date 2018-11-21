beac33e5fe62410dfc91c4db41c7a668a6a29c3d
tlnguyen2018 <tlnguyen3839@gmail.com>
2018-11-21 07:41:27 +0000

    simple shell

---
+++ shell.h
@@ -0,0 +1,61 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+#define EXIT_BUILTIN 1
+#define NON_BUILTIN 0
+
+extern char **environ;
+
+extern int errno;
+
+/**
+ * struct built_in - struct for matching commands to built in commands
+ * @command: the string matching the command to execute
+ * @built_cmd: the built in command to call if command matches
+ * Description: Special built in commands for custom shell
+ */
+typedef struct built_in
+{
+	char *command;
+	char (*built_cmd)(char **, char **);
+} do_built;
+
+char *check_path(char *);
+char **arg_list(int);
+int error_call(int, char *, char **);
+
+void free_double(char **);
+
+char **strtow(char *, char);
+int word_counter(char *, char);
+char *_malloc(int n, char **);
+
+char exit_builtin(char **, char **);
+char env_builtin(char **, char **);
+
+int _strlen(char *);
+int _strcmp(char *, char *);
+char *_strcat_dir(char *, char *);
+int _atoi(char *);
+
+char **build_path(char *);
+void print_dir(char *);
+void print_env(char **);
+char *_getenv(char *);
+char *cut_env(char *);
+
+int print_str(char *);
+int builtin_finder(char **);
+
+#endif
